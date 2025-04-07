5a56ec875d7bf91a411bbd9f18e5e871df342eaf
Hasireddy <malikireddy.uma@gmail.com>
2025-04-07 09:13:14 +0200

    MaterialUI icon added to the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,22 @@
+import { AppBar,Container } from "@mui/material";
+import StoreIcon from '@mui/icons-material/Store';
+import './Header.css';
+
 function Header()
 {
     return (
         <div>
-            <h1>My Store</h1>
+            <AppBar color={"success"} className="appbar" position="static">
+                <Container maxWidth="xl">
+                    <StoreIcon style={{fontSize:"80px"}}/>
+                    <h1>Page1</h1>
+                    <h1>Page2</h1>
+                    <h1>Page3</h1>
+                    <h1>Page4</h1>
+                </Container>
+            </AppBar>
         </div>
     )
 }
+
+export default Header;
