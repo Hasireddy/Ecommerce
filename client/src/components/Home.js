cc04774201b5f011e9c7fb2675b813efe99b20b6
Hasireddy <malikireddy.uma@gmail.com>
2025-04-09 11:27:12 +0200

    Background image replaced with new one

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,11 @@
 import {Typography,Box,Button,Container} from '@mui/material';
-import cover from '../Assets/landingpage.jpg'
+import cover from '../Assets/landing2.jpg'
 
 function Home(){
     return(
         <>
-         <Container sx={{display:'flex',flexDirection:'column',alignItems:'center',minHeight:'100vh',margin:'auto',textAlign:'center',justifyContent:'center',backgroundImage:`url(${cover})`,maxWidth:'100%'}}>
+        <Box sx={{backgroundImage:`url(${cover})`}}>
+         <Container sx={{display:'flex',flexDirection:'column',alignItems:'center',minHeight:'100vh',margin:'auto',textAlign:'center',justifyContent:'center',maxWidth:'100%'}}>
             <Box sx={{marginTop:'6rem',padding:'1rem',maxWidth:'100%',overflow:'hidden'}}>
                 <Typography sx={{
                     fontSize:'3rem',
@@ -45,6 +46,7 @@ function Home(){
                 </Box>
             </Box>  
         </Container> 
+        </Box>
         </>
     )
 }
